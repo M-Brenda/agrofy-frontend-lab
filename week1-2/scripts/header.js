6f6bc84f0a8132a92596dfc574e9605017c1472e
M-Brenda <brenda_manzano@hotmail.com>
2020-02-28 23:54:03 -0300

    Header terminado

--- week1-2/scripts/header.js
+++ week1-2/scripts/header.js
@@ -1,18 +1,3 @@
-let body = document.querySelector("body");
-let header = document.querySelector("header");
-
-
-/*
-function scrollheader () {
-   
-    if (body.scrollTop > 1) {
-        header.className="scroll";
-    }
-    else{
-        header.classList.remove("scroll")
-    }
-}
-*/
 
 window.onscroll = function() {scrollFunction()};
 
@@ -22,4 +7,4 @@ function scrollFunction() {
   } else {
     document.getElementById("header").classList.remove("scroll")
   }
-}
+} 
